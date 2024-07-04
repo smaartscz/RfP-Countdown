449500c241bd879499c5b4ec7ee963b7831c3905
smaartscz <65630155+smaartscz@users.noreply.github.com>
2024-07-04 11:45:33 +0200

    Update streak.py

--- modules/streak.py
+++ modules/streak.py
@@ -1,7 +1,8 @@
 import modules.configuration as configuration
 from modules.f_time import get_time
-
+import modules.colors as colors
 def increase():
+    print(colors.yellow + "Wow, good job! Increasing streak." + colors.reset)
     current_streak = int(configuration.get_value(section="Statistics", key="streak"))
     new_streak = int(current_streak) + 1
     if new_streak >= current_streak:
@@ -11,4 +12,6 @@ def increase():
     configuration.save(section="Statistics",key="last_ping", value=get_time())
 
 def reset():
+    streak = int(configuration.get_value(section="Statistics", key="streak"))
+    print(colors.red + f"Too bad! You have lost your streak of {streak} days." + colors.reset)
     configuration.save(section="Statistics", key="streak", value="0")
